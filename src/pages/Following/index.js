5f5885fdca71f01e2e72da87a9a3d37bbc7c592e
vanlinh00 <vanlinh20192019@gmail.com>
2026-03-15 21:23:45 +0700

    now croll left tab

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -97,21 +97,60 @@ const MOCK_DATA = [
             tick: true,
         },
     },
+    {
+        id: 2,
+        file_url: 'https://v16-webapp.tiktok.com/...',
+        user: {
+            nickname: 'denvau',
+            full_name: 'Đen Vâu',
+            avatar: 'https://p16-sign-va.tiktokcdn.com/...',
+            tick: true,
+        },
+    },
+    {
+        id: 2,
+        file_url: 'https://v16-webapp.tiktok.com/...',
+        user: {
+            nickname: 'denvau',
+            full_name: 'Đen Vâu',
+            avatar: 'https://p16-sign-va.tiktokcdn.com/...',
+            tick: true,
+        },
+    },
+    {
+        id: 2,
+        file_url: 'https://v16-webapp.tiktok.com/...',
+        user: {
+            nickname: 'denvau',
+            full_name: 'Đen Vâu',
+            avatar: 'https://p16-sign-va.tiktokcdn.com/...',
+            tick: true,
+        },
+    },
+    {
+        id: 2,
+        file_url: 'https://v16-webapp.tiktok.com/...',
+        user: {
+            nickname: 'denvau',
+            full_name: 'Đen Vâu',
+            avatar: 'https://p16-sign-va.tiktokcdn.com/...',
+            tick: true,
+        },
+    },
+    {
+        id: 2,
+        file_url: 'https://v16-webapp.tiktok.com/...',
+        user: {
+            nickname: 'denvau',
+            full_name: 'Đen Vâu',
+            avatar: 'https://p16-sign-va.tiktokcdn.com/...',
+            tick: true,
+        },
+    },
 ];
 
 function Following() {
     const [users, setUsers] = useState(MOCK_DATA); // Để mặc định là mock data để thấy giao diện ngay
-
-    useEffect(() => {
-        fetch('https://tiktok.fullstack.edu.vn/api/videos?type=for-you&page=2')
-            .then((res) => res.json())
-            .then((res) => {
-                if (res.data) {
-                    setUsers(res.data);
-                }
-            })
-            .catch(() => console.log('Dùng dữ liệu fake do lỗi kết nối'));
-    }, []);
 
     return (
         <div className={cx('container')}>
