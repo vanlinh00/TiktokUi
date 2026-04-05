2c63e400f66cceaa3167aff8da5cdc4779605ab2
vanlinh00 <vanlinh20192019@gmail.com>
2026-04-05 22:28:25 +0700

    update

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import { SearchIcon, HomeIcon } from '~/components/Icons';
+import config from '~/config';
+import images from '~/assets/images';
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Search() {
-    return <h2>Search page</h2>;
+    const [query, setQuery] = useState('');
+
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('search-panel')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </Link>
+
+                <div className={cx('search-box')}>
+                    <SearchIcon className={cx('search-icon')} />
+                    <input
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        placeholder="Search"
+                        spellCheck={false}
+                    />
+                </div>
+
+                <button type="button" className={cx('for-you-btn')}>
+                    <span className={cx('for-you-icon')}>
+                        <HomeIcon />
+                    </span>
+                    <span>For You</span>
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Search;
